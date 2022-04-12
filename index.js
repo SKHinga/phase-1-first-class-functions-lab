d495eadb9c2b99bf06adfbb0ef96b14a4a3643a5
Samuel Hinga <samuelhinga200@gmail.com>
2022-04-12 10:54:42 +0300

    commit tree 34b60f2a6f9990e4a9212203ac60346ad6f03cf9

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = function (){
+  return drivers.slice(0,2);
+  }
+returnFirstTwoDrivers(drivers);
+
+const returnLastTwoDrivers = function (){
+  return drivers.slice(2);
+}
+returnLastTwoDrivers(drivers);
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(amount){
+  return function (){
+    return amount**2
+  }
+}
+createFareMultiplier(4);
+const fareQuintupler = createFareMultiplier(5);
+function fareDoubler(money){
+  return money*2
+}
+fareDoubler(10);
+function fareTripler (nauli){
+  return nauli*3
+}
+fareTripler(12);
+function selectDifferentDrivers (){
+  return returnFirstTwoDrivers();
+  return returnLastTwoDrivers();
+};
+function selectDifferentDrivers(drivers, returnDriver){
+  return returnDriver(drivers);
+};
